428b1088f62971dd461d3fdd4882162027586e39
ife <Bioes2007@gmail.com>
2025-06-18 10:45:48 +0100

    third commit

--- FRONTEND1/src/app/page.tsx
+++ FRONTEND1/src/app/page.tsx
@@ -3,7 +3,7 @@ import AgentRegistrationpage from "./(registration)/agent/page";
 import CreateAccount from "./(registration)/user/page";
 import Dashboard from "./dashboard/page";
 import RegistrationPage from "./(registration)/user/page";
-import LoanPage from "./machine_learning/loan_eligibility_predictor/page";
+import LoanPage from "./loan_eligibility_predictor/page";
 import Homely from "./load/page";
 export default function Home() {
   return (
